512f25d49c275b8afdefa61f23f8d4ef4db366c5
Syam Kumar Chintakayala <syamkumar6845@gmail.com>
2024-08-27 23:38:25 +0530

    added exception handling

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ function displayVideo() {
         document.getElementById('videoContainer').innerHTML = `<iframe width="560" height="315" src="${embedUrl}" title="YouTube video player"
         frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`;
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen
+        sandbox="allow-same-origin allow-scripts"></iframe>`;
     }
 }
 
